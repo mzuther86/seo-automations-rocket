ef7ed5c0d404804220caa2bca7f475cabb8851a8
mzuther86 <mzlev@web.de>
2025-06-24 13:49:02 +0200

    z

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", 
+  base: "/seo-automations-rocket/",
     server: {
         host: "::",
             port: 8080,
